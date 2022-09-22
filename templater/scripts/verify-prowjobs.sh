76cf3bcccf20126e052ad53f5125c9f7bc21d8d7
Daniel Budris <budris@amazon.com>
2022-09-22 17:34:16 -0400

    add git diff to debug prow job output

--- templater/scripts/verify-prowjobs.sh
+++ templater/scripts/verify-prowjobs.sh
@@ -7,6 +7,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 DIFF_LINE_COUNT=$(git diff $REPO_ROOT/jobs | wc -l)
 if [ $DIFF_LINE_COUNT -ne 0 ]; then
     CHANGED_FILES=$(git diff --name-only $REPO_ROOT/jobs)
+    git diff $REPO_ROOT/jobs
     echo "\n❌ Detected discrepancies between generated and expected Prowjobs!"
     echo "The following generated files need to be checked in:\n"
     echo "${CHANGED_FILES}\n" | tr ' ' '\n'
