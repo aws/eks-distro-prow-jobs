d77058a516fd29f6cc4face9c46aa6c662458d55
Jackson West <jgw@amazon.com>
2023-01-24 09:40:42 -0600

    Refactors templater to support adding additional easier (#466)

--- templater/jobs/utils/utils.go
+++ templater/jobs/utils/utils.go
@@ -30,6 +30,16 @@ var golangVersions = []string{
 	"1-19",
 }
 
+var pythonVersions = []string{
+	"3-7",
+	"3-9",
+}
+
+var alVersions = []string{
+	"2",
+	"2022",
+}
+
 func GetJobsByType(repos []string, jobType string) (map[string]map[string]types.JobConfig, error) {
 	jobsListByType := map[string]map[string]types.JobConfig{}
 	for _, repo := range repos {
@@ -46,69 +56,157 @@ func GetJobsByType(repos []string, jobType string) (map[string]map[string]types.
 	return jobsListByType, nil
 }
 
+func AppendMap(current map[string]interface{}, new map[string]interface{}) map[string]interface{} {
+	newMap := map[string]interface{}{}
+	for k, v := range current {
+		newMap[k] = v
+	}
+	for k, v := range new {
+		newMap[k] = v
+	}
+
+	return newMap
+}
+
+func AddALVersion(fileName string, data map[string]interface{}) map[string]map[string]interface{} {
+	jobList := map[string]map[string]interface{}{}
+	if !strings.Contains(fileName, "al-X") {
+		return jobList
+	}
+
+	for _, version := range alVersions {
+		alVersionFileName := strings.ReplaceAll(fileName, "al-X", "al-"+version)
+		jobList[alVersionFileName] = AppendMap(data, map[string]interface{}{
+			"alVersion": version,
+		})
+	}
+
+	return jobList
+}
+
+func AddGolangVersion(fileName string, data map[string]interface{}) map[string]map[string]interface{} {
+	jobList := map[string]map[string]interface{}{}
+	if !strings.Contains(fileName, "golang-1-X") {
+		return jobList
+	}
+
+	for _, version := range golangVersions {
+		golangVersionFileName := strings.ReplaceAll(fileName, "golang-1-X", "golang-"+version)
+		goVersion := strings.Replace(version, "-", ".", 1)
+		jobList[golangVersionFileName] = AppendMap(data, map[string]interface{}{
+			"jobGoVersion":  version,
+			"golangVersion": goVersion,
+		})
+	}
+
+	return jobList
+}
+
+func AddPythonVersion(fileName string, data map[string]interface{}) map[string]map[string]interface{} {
+	jobList := map[string]map[string]interface{}{}
+	if !strings.Contains(fileName, "python-3-X") {
+		return jobList
+	}
+
+	for _, version := range pythonVersions {
+		pythonVersionFileName := strings.ReplaceAll(fileName, "python-3-X", "python-"+version)
+		pythonVersion := strings.Replace(version, "-", ".", 1)
+		jobList[pythonVersionFileName] = AppendMap(data, map[string]interface{}{
+			"jobPythonVersion": version,
+			"pythonVersion":    pythonVersion,
+		})
+	}
+
+	return jobList
+}
+
+func AddReleaseBranch(fileName string, data map[string]interface{}) map[string]map[string]interface{} {
+	jobList := map[string]map[string]interface{}{}
+	if !strings.Contains(fileName, "1-X") {
+		return jobList
+	}
+
+	for i, releaseBranch := range releaseBranches {
+		releaseBranchBasedFileName := strings.ReplaceAll(fileName, "1-X", releaseBranch)
+		otherReleaseBranches := append(append([]string{}, releaseBranches[:i]...),
+			releaseBranches[i+1:]...)
+		jobList[releaseBranchBasedFileName] = AppendMap(data, map[string]interface{}{
+			"releaseBranch":        releaseBranch,
+			"otherReleaseBranches": strings.Join(otherReleaseBranches, "|"),
+		})
+
+		// If latest release branch, check if the release branch dir exists before executing cmd
+		// This allows us to experiment with adding prow jobs for new branches without failing other runs
+		if len(releaseBranches)-1 == i {
+			jobList[releaseBranchBasedFileName]["latestReleaseBranch"] = true
+		}
+	}
+
+	return jobList
+}
+
+func RunMappers(jobsToData map[string]map[string]interface{}, mappers []func(string, map[string]interface{}) map[string]map[string]interface{}) {
+	if len(mappers) == 0 {
+		return
+	}
+
+	for fileName, data := range jobsToData {
+		newJobList := mappers[0](fileName, data)
+		if len(newJobList) == 0 {
+			continue
+		}
+
+		for k, v := range newJobList {
+			jobsToData[k] = v
+			if _, ok := data["templateFileName"]; !ok {
+				jobsToData[k]["templateFileName"] = fileName
+			}
+		}
+		delete(jobsToData, fileName)
+	}
+
+	RunMappers(jobsToData, mappers[1:])
+}
+
 func UnmarshalJobs(jobDir string) (map[string]types.JobConfig, error) {
-	jobList := map[string]types.JobConfig{}
 
 	files, err := ioutil.ReadDir(jobDir)
 	if err != nil {
 		return nil, fmt.Errorf("error reading job directory %s: %v", jobDir, err)
 	}
 
+	var mappers []func(string, map[string]interface{}) map[string]map[string]interface{}
+	mappers = append(mappers, AddALVersion, AddGolangVersion, AddPythonVersion, AddReleaseBranch)
+
+	jobsToData := map[string]map[string]interface{}{}
 	for _, file := range files {
-		fileName := file.Name()
-		filePath := filepath.Join(jobDir, fileName)
-		if strings.Contains(fileName, "golang-1-X") {
-			for _, version := range golangVersions {
-				versionBasedFileName := strings.ReplaceAll(fileName, "1-X", version)
-				goVersion := strings.Replace(version, "-", ".", 1)
-				data := map[string]interface{}{
-					"jobGoVersion":  version,
-					"golangVersion": goVersion,
-				}
-
-				jobConfig, err := GenerateJobConfig(data, filePath)
-
-				if err != nil {
-					return nil, fmt.Errorf("%v", err)
-				}
-
-				jobList[versionBasedFileName] = jobConfig
-			}
-		} else if strings.Contains(fileName, "1-X") {
-			for i, releaseBranch := range releaseBranches {
-				releaseBranchBasedFileName := strings.ReplaceAll(fileName, "1-X", releaseBranch)
-				otherReleaseBranches := append(append([]string{}, releaseBranches[:i]...),
-					releaseBranches[i+1:]...)
-				data := map[string]interface{}{
-					"releaseBranch":        releaseBranch,
-					"otherReleaseBranches": strings.Join(otherReleaseBranches, "|"),
-				}
-
-				jobConfig, err := GenerateJobConfig(data, filePath)
-				if err != nil {
-					return nil, fmt.Errorf("%v", err)
-				}
-
-				// If latest release branch, check if the release branch dir exists before executing cmd
-				// This allows us to experiment with adding prow jobs for new branches without failing other runs
-				if len(releaseBranches)-1 == i {
-					for j, command := range jobConfig.Commands {
-						jobConfig.Commands[j] = "if [ -d $PROJECT_PATH/$RELEASE_BRANCH ]; then " + command + "; fi"
-					}
-				}
-
-				jobList[releaseBranchBasedFileName] = jobConfig
-			}
-		} else {
-			jobConfig, err := GenerateJobConfig(nil, filePath)
-			if err != nil {
-				return nil, fmt.Errorf("%v", err)
-			}
-			jobList[fileName] = jobConfig
-		}
+		jobsToData[file.Name()] = map[string]interface{}{}
 	}
 
-	return jobList, nil
+	RunMappers(jobsToData, mappers)
+
+	finalJobList := map[string]types.JobConfig{}
+	for fileName, data := range jobsToData {
+		templateFileName := fileName
+		if name, ok := data["templateFileName"]; ok {
+			templateFileName = name.(string)
+		}
+
+		jobConfig, err := GenerateJobConfig(data, filepath.Join(jobDir, templateFileName))
+		if err != nil {
+			return nil, fmt.Errorf("%v", err)
+		}
+
+		if latest, ok := data["latestReleaseBranch"]; ok && latest.(bool) {
+			for j, command := range jobConfig.Commands {
+				jobConfig.Commands[j] = "if [ -d $PROJECT_PATH/$RELEASE_BRANCH ]; then " + command + "; fi"
+			}
+		}
+
+		finalJobList[fileName] = jobConfig
+
+	}
+	return finalJobList, nil
 }
 
 func ExecuteTemplate(templateContent string, data interface{}) ([]byte, error) {
