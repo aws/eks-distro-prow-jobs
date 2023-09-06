68f0829ba32c210df2e742f8dee0654f30940f54
Sajia Zafreen <42818305+zafs23@users.noreply.github.com>
2023-09-06 14:31:09 -0700

    Request ephemeral-storage for 1-27-test-presubmit (#539)

--- linter/main.go
+++ linter/main.go
@@ -121,9 +121,6 @@ func PresubmitBucketCheck(jc *JobConstants) presubmitCheck {
 		if strings.Contains(presubmitConfig.JobBase.Name, "builder-base-tooling-presubmit") {
 			return true, 0, ""
 		}
-		if strings.Contains(presubmitConfig.JobBase.Name, "kubernetes-1-27-test-presubmit") {
-			return true, 0, ""
-		}
 		if presubmitConfig.JobBase.UtilityConfig.DecorationConfig.GCSConfiguration.Bucket != jc.Bucket {
 			return false, findLineNumber(fileContentsString, "bucket:"), fmt.Sprintf(`Incorrect bucket configuration, please configure S3 bucket as => bucket: %s`, jc.Bucket)
 		}
@@ -145,9 +142,6 @@ func PresubmitClusterCheck(jc *JobConstants) presubmitCheck {
 		if strings.Contains(presubmitConfig.JobBase.Name, "builder-base-tooling-presubmit") {
 			return true, 0, ""
 		}
-		if strings.Contains(presubmitConfig.JobBase.Name, "kubernetes-1-27-test-presubmit") {
-			return true, 0, ""
-		}
 		if presubmitConfig.JobBase.Cluster != jc.Cluster {
 			return false, findLineNumber(fileContentsString, "cluster:"), fmt.Sprintf(`Incorrect cluster configuration, please configure cluster as => cluster: "%s"`, jc.Cluster)
 		}
@@ -167,9 +161,6 @@ func PostsubmitClusterCheck(jc *JobConstants) postsubmitCheck {
 func PresubmitServiceAccountCheck(jc *JobConstants) presubmitCheck {
 	return presubmitCheck(func(presubmitConfig config.Presubmit, fileContentsString string) (bool, int, string) {
 		if strings.Contains(presubmitConfig.JobBase.Name, "builder-base-tooling-presubmit") {
-			return true, 0, ""
-		}
-		if strings.Contains(presubmitConfig.JobBase.Name, "kubernetes-1-27-test-presubmit") {
 			return true, 0, ""
 		}
 		if presubmitConfig.JobBase.Spec.ServiceAccountName != jc.ServiceAccountName {
