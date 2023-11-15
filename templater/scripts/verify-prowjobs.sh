e787028dc7b7b4ad195dfbad6bff23b5ff2f761e
Sajia Zafreen <42818305+zafs23@users.noreply.github.com>
2023-11-15 11:51:14 -0800

    Remove 1.23 Prowjobs K8 presubmit tests and Update CNI-plugin tests triggeres (#564)
    * Remove 1.23 Prowjobs and Update CNI-plugin tests triggeres
    
    * keep the 1-23 branch
    
    * test
    
    * get rid of the triggers from 1-23 and change verify-prowjob script to exclude 1-23
    
    * Remove 1.23 jobs
    
    * update script
    
    * update
    
    * update script

--- templater/scripts/verify-prowjobs.sh
+++ templater/scripts/verify-prowjobs.sh
@@ -1,13 +1,15 @@
+#!/usr/bin/env bash
+
 set -e
 set -u
 set -o pipefail
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-DIFF_LINE_COUNT=$(git diff --name-only $REPO_ROOT/jobs ':(exclude)*-1-23-*' | wc -l)
+DIFF_LINE_COUNT=$(git diff --name-only $REPO_ROOT/jobs ':(exclude,top)*-1-23-*' | wc -l)
 if [ $DIFF_LINE_COUNT -ne 0 ]; then
-    CHANGED_FILES=$(git diff --name-only $REPO_ROOT/jobs ':(exclude)*-1-23-*')
-    git diff $REPO_ROOT/jobs ':(exclude)*-1-23-*'
+    CHANGED_FILES=$(git diff --name-only $REPO_ROOT/jobs ':(exclude,top)*-1-23-*')
+    git diff $REPO_ROOT/jobs ':(exclude,top)*-1-23-*'
     echo "\n❌ Detected discrepancies between generated and expected Prowjobs!"
     echo "The following generated files need to be checked in:\n"
     echo "${CHANGED_FILES}\n" | tr ' ' '\n'
