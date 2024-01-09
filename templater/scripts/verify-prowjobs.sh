2991cd1df34fcc595b05b21818a79d1205f47fd6
Sajia Zafreen <42818305+zafs23@users.noreply.github.com>
2024-01-09 11:01:28 -0800

    Generalize supported branches for K8 patches (#581)

--- templater/scripts/verify-prowjobs.sh
+++ templater/scripts/verify-prowjobs.sh
@@ -6,10 +6,10 @@ set -o pipefail
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-DIFF_LINE_COUNT=$(git diff --name-only $REPO_ROOT/jobs ':(exclude,top)*-1-23-*' | wc -l)
+DIFF_LINE_COUNT=$(git diff --name-only $REPO_ROOT/jobs | wc -l)
 if [ $DIFF_LINE_COUNT -ne 0 ]; then
-    CHANGED_FILES=$(git diff --name-only $REPO_ROOT/jobs ':(exclude,top)*-1-23-*')
-    git diff $REPO_ROOT/jobs ':(exclude,top)*-1-23-*'
+    CHANGED_FILES=$(git diff --name-only $REPO_ROOT/jobs)
+    git diff $REPO_ROOT/jobs
     echo "\n❌ Detected discrepancies between generated and expected Prowjobs!"
     echo "The following generated files need to be checked in:\n"
     echo "${CHANGED_FILES}\n" | tr ' ' '\n'
