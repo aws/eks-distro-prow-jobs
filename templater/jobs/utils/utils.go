05d986abea6b136d43c73ee0fbea0a9e6b846d11
Vineeth Bandi <banvinee@amazon.com>
2024-06-26 13:22:43 -0500

    update prow jobs for 1.25 and 1.26 deprecation

--- templater/jobs/utils/utils.go
+++ templater/jobs/utils/utils.go
@@ -14,8 +14,6 @@ import (
 )
 
 var releaseBranches = []string{
-	"1-25",
-	"1-26",
 	"1-27",
 	"1-28",
 	"1-29",
@@ -25,6 +23,8 @@ var releaseBranches = []string{
 var k8releaseBranches = []string{
 	"1-23",
 	"1-24",
+	"1-25",
+	"1-26",
 }
 
 var golangVersions = []string{
