ed6dc50891cbd58f9906e712481ba8bb7ad91757
Vineeth Bandi <banvinee@amazon.com>
2025-01-14 10:37:19 -0600

    deprecate 1.27 prowjobs

--- templater/jobs/utils/utils.go
+++ templater/jobs/utils/utils.go
@@ -14,7 +14,6 @@ import (
 )
 
 var releaseBranches = []string{
-	"1-27",
 	"1-28",
 	"1-29",
 	"1-30",
@@ -27,6 +26,7 @@ var k8releaseBranches = []string{
 	"1-24",
 	"1-25",
 	"1-26",
+	"1-27",
 }
 
 var golangVersions = []string{
